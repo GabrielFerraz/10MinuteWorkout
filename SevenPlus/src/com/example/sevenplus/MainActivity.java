4cd9162b80e5d1e6c9c91759b01a0da03fce932b
GenesioValois <genas96@gmail.com>
2013-11-19 20:14:59 -0300

    upando novas ativities

--- SevenPlus/src/com/example/sevenplus/MainActivity.java
+++ SevenPlus/src/com/example/sevenplus/MainActivity.java
@@ -5,32 +5,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Button botao = (Button)findViewById(R.id.button1);
-        botao.setOnClickListener(new View.OnClickListener() {
-			
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);	
+		ImageButton teste = (ImageButton) findViewById( R.id.imageButton1 ); 
+		
+		teste.setOnClickListener(new OnClickListener(){
+
 			@Override
 			public void onClick(View arg0) {
-				Intent teste = new Intent(arg0.getContext(), Instrucoes.class);
+				// TODO Auto-generated method stub
+				Intent teste = new Intent(arg0.getContext(), WorkoutActivity.class);
 				startActivity(teste);
 			}
+			
 		});
-    }
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
+	  
+	  
 }
